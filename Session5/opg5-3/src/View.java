f5faa8d3bf60c4e7d18cebffb2ec47110414dc27
Baris Cankül <Bariscankul2003@hotmail.com>
2024-02-20 21:52:32 +0100

    18.58 20.02.2024

---
+++ Session5/opg5-3/src/View.java
@@ -0,0 +1,48 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.util.converter.NumberStringConverter;
+
+public class View
+{
+  @FXML
+  TextField usernameField;
+  @FXML
+  TextField passwordField;
+  @FXML
+  TextField passwordRepeatField;
+  @FXML
+  TextField ageField;
+
+  private ViewModel viewModel;
+
+  public View(ViewModel viewModel)
+  {
+    this.viewModel = viewModel;
+  }
+
+  public void initialize()
+  {
+    usernameField.setTextFormatter(new TextFormatter<String>(change -> {
+      String newText = change.getControlNewText();
+      if (newText.length() <= 20) {
+        return change;
+      }
+      return null;
+    }));
+    ageField.setTextFormatter(new TextFormatter<Integer>(change -> {
+      String newText = change.getControlNewText();
+      if (newText.matches("\\d*")) {
+        return change;
+      }
+      return null;
+    }));
+    passwordField.textProperty().bindBidirectional(viewModel.passwordProperty());
+    passwordRepeatField.textProperty().bindBidirectional(viewModel.passwordRepeatProperty());
+  }
+
+  public void onAddButtonPressed()
+  {
+    viewModel.addUser();
+  }
+}
