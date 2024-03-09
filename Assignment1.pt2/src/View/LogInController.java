a725ec94cf83551b329afa0a76440d36ef29425a
Baris Cankül <147693702+BarisCnkl@users.noreply.github.com>
2024-03-09 15:29:32 +0100

    16:48 14-02-2024

---
+++ Assignment1.pt2/src/View/LogInController.java
@@ -0,0 +1,14 @@
+package View;
+
+import ViewModel.LogInViewModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LogInController
+{
+  @FXML TextField UserIdField;
+  @FXML Button LogInButton;
+
+  private LogInViewModel logInViewModel;
+}
